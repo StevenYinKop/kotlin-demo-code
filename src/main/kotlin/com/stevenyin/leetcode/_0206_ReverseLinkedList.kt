3e0e0c5f7c64234a956ea88383b0a6969e79456d
Steven.Yin <yinzifancn@gmail.com>
2021-08-22 22:37:36 +0800

    update leetcode

---
+++ src/main/kotlin/com/stevenyin/leetcode/_0206_ReverseLinkedList.kt
@@ -0,0 +1,24 @@
+package com.stevenyin.leetcode
+
+/**
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class _0206_ReverseLinkedList {
+    fun reverseList(head: ListNode?): ListNode? {
+        if (head == null || head.next == null) return head
+        val newHead = reverseList(head.next)
+        head.next!!.next = head
+        head.next = null
+        return newHead
+    }
+}
---
+++ src/main/kotlin/com/stevenyin/leetcode/_0283_MoveZeros.kt
@@ -0,0 +1,46 @@
+package com.stevenyin.leetcode
+
+/**
+ * https://leetcode.com/problems/move-zeroes/
+ * Given an integer array nums,
+ * move all 0's to the end of it while maintaining
+ * the relative order of the non-zero elements.
+ *
+    Note that you must do this in-place without making a copy of the array.
+
+    Example 1:
+    Input: nums = [0,1,0,3,12]
+    Output: [1,3,12,0,0]
+
+    Example 2:
+    Input: nums = [0]
+    Output: [0]
+ */
+class _0283_MoveZeros {
+    fun moveZeroes(nums: IntArray): Unit {
+        var k = 0;
+        for (i in nums.indices) {
+            if (nums[i] != 0) {
+                if (i != k) {
+                    swap(nums, k++, i);
+                } else {
+                    k++;
+                }
+            }
+        }
+    }
+    private fun swap(nums: IntArray, i: Int, j: Int) {
+        val temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
+
+// 26, 27, 80, 75, 88, 215, 167,
+// 11
+// 字符串 125, 344, 345
+// 空字符串
+// 字符的定义
+// 大小写问题
+
+// Slide window  209, 3, 438, 76
