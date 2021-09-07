78d5d73e40df7db256c3a857d9e5c282da87d989
Steven.Yin <yinzifancn@gmail.com>
2021-09-08 01:26:13 +0800

    1. add design pattern

---
+++ src/main/kotlin/cc/stevenyin/design_pattern/_01_06_LawofDemeter.kt
@@ -0,0 +1,46 @@
+package cc.stevenyin.design_pattern
+
+import kotlin.random.Random
+
+class _01_06_LawofDemeter {
+
+    class Wizard {
+        val rand: Random = Random(System.currentTimeMillis())
+
+        fun first(): Int {
+            println("执行第一步...")
+            return rand.nextInt(100)
+        }
+
+        fun second(): Int {
+            println("执行第二步...")
+            return rand.nextInt(100)
+        }
+
+        fun third(): Int {
+            println("执行第三步...")
+            return rand.nextInt(100)
+        }
+    }
+
+    class InstallSoftware {
+        fun installWizard(wizard: Wizard) {
+            val first = wizard.first()
+            if (first > 50) {
+                val second = wizard.second()
+                if (second > 50) {
+                    val third = wizard.third()
+                    if (third > 50) {
+                        wizard.first()
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+fun main() {
+    val invoker = _01_06_LawofDemeter.InstallSoftware()
+    invoker.installWizard(_01_06_LawofDemeter.Wizard())
+}
