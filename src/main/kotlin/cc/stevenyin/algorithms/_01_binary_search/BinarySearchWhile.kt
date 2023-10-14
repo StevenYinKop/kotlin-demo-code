64658b6df797f26d77a071ceb05bd152679fd29c
Steven.Yin <20436656+StevenYinKop@users.noreply.github.com>
2023-10-15 01:04:16 +0800

    Optimize binarySearch

--- src/main/kotlin/cc/stevenyin/algorithms/_01_binary_search/BinarySearchWhile.kt
+++ src/main/kotlin/cc/stevenyin/algorithms/_01_binary_search/BinarySearchWhile.kt
@@ -1,25 +1,33 @@
 package cc.stevenyin.algorithms._01_binary_search
 
+/**
+ * Binary search to find the index of target in a sorted array.
+ *
+ * @param array The sorted array to search.
+ * @param target The value to search for.
+ * @return The index of target if found, or the negation of the insertion point otherwise.
+ */
 fun <T: Comparable<T>> binarySearch(array: Array<T>, target: T): Int {
-    var left: Int = 0
-    var right = array.size - 1
-    while (left <= right) {
-        val mid = (left + right) / 2
-        if (array[mid] == target) {
-            return mid
-        }
-        if (array[mid] > target) {
-            right = mid - 1
-        } else {
-            left = mid + 1
+
+    var low = 0
+    var high = array.size - 1
+
+    while (low < high) {
+        val midIndex = (low + high) / 2
+        val mid = array[midIndex]
+        when {
+            mid < target -> low = midIndex + 1
+            mid > target -> high = midIndex - 1
+            else -> return midIndex
         }
     }
-    return -1
+    return -low - 1  // Not found, would insert at -low-1
 }
 fun main() {
-    println(binarySearch(arrayOf(10, 20, 30, 50, 80, 99, 100), 2))
-    println(binarySearch(arrayOf(10, 20, 30, 50, 80, 99, 100), 10))
-    println(binarySearch(arrayOf(10, 20, 30, 50, 80, 99, 100), 100))
-    println(binarySearch(arrayOf(10, 20, 30, 50, 80, 99, 100), 101))
-    println(binarySearch(arrayOf(10, 20, 30, 50, 80, 99, 100), 98))
+    val testData = arrayOf(10, 20, 30, 50, 80, 99, 100)
+    assert(binarySearch(testData, 2) == -1)
+    assert(binarySearch(testData, 10) == 0)
+    assert(binarySearch(testData, 100) == 6)
+    assert(binarySearch(testData, 101) == -1)
+    assert(binarySearch(testData, 98) == -1)
 }
