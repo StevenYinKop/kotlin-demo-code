a673d61e2c0087d3cdcbdefe100519fb35985fa3
Steven.Yin <20436656+StevenYinKop@users.noreply.github.com>
2023-10-16 23:54:15 +0800

    Add #147,#881

---
+++ src/main/kotlin/cc/stevenyin/leetcode/_0147_InsertionSortList.kt
@@ -0,0 +1,30 @@
+package cc.stevenyin.leetcode
+
+/**
+ * https://leetcode.com/problems/insertion-sort-list/
+ * Given the head of a singly linked list, sort the list using insertion sort, and return the sorted list's head.
+ *
+ * The steps of the insertion sort algorithm:
+ *
+ * Insertion sort iterates, consuming one input element each repetition and growing a sorted output list.
+ * At each iteration, insertion sort removes one element from the input data, finds the location it belongs within the sorted list and inserts it there.
+ * It repeats until no input elements remain.
+ * Example 1:
+ * Input: head = [4,2,1,3]
+ * Output: [1,2,3,4]
+ *
+ * Example 2:
+ * Input: head = [-1,5,3,4,0]
+ * Output: [-1,0,3,4,5]
+ *
+ * Constraints:
+ *
+ * The number of nodes in the list is in the range [1, 5000].
+ * -5000 <= Node.val <= 5000
+ *
+ */
+class _0147_InsertionSortList {
+    fun insertionSortList(head: ListNode?): ListNode? {
+        return null
+    }
+}
