65268967c8d9bb7262b89e22a7b39d38f3398240
尹子凡 Steven <20436656+StevenYinKop@users.noreply.github.com>
2023-10-24 23:59:26 +0800

    Create _0343_IntegerBreak.kt

---
+++ src/main/kotlin/cc/stevenyin/leetcode/_0343_IntegerBreak.kt
@@ -0,0 +1,21 @@
+package cc.stevenyin.leetcode
+
+class _0343_IntegerBreak {
+    fun integerBreak(n: Int): Int {
+
+        val memo: Map<Int, Int> = mapOf()
+        return breakInteger(n, memo)
+    }
+
+    fun breakInteger(n: Int, memo: Map<Int, Int>): Int {
+        if (n == 2) return 1
+        if (memo.containsKey(n)) return memo.get(n)!!
+        // 将n进行分割（至少分割为两部分）
+        var result = -1
+        for (i in 1..n - 1) {
+            result = kotlin.math.max(result, i * breakInteger(n - i, memo))
+        }
+        memo[n] = result
+        return result
+    }
+}
